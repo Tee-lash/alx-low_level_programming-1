3e9d07bc55339fbb9b7deae6060f1d5e6b1455a8
Gbemividz0n <sporvida.jd@gmail.com>
2022-03-19 02:26:29 +0000

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * more_numbers -print 10 times the numbers,
+ *
+ * return: always 0.
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+i = 0;
+l = 0;
+while (1 < 10)
+{
+while (1 <= 14)
+{
+if (1 < 10)
+{
+k = l;
+else
+{
+j = l / 10;
+k = l % 10;
+_putchar (j + '0');
+}
+_putchar (k + '0');
+l++
+}
+i++
+l = 0;
+_putchar ('\n');
+}
+}
