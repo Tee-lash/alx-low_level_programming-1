04e530f4ddb34b7c696e321c0ed404e6e032d6be
Gbemividz0n <sporvida.jd@gmail.com>
2022-03-19 02:32:04 +0000

    push

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_most_numbers -print number
  *
- * return: alway 0.
+ * return: always 0.
  */
 void print_most_numbers(void)
 {
@@ -11,9 +11,9 @@ int i;
 for (i = 0; i < 10; i++)
 {
 if (i != 2 && i != 4)
-{
-															_putchar (i + '0');
-																	}
+
+{													_putchar (i + '0');
 }
+}												
 _putchar ('\n');
 }
