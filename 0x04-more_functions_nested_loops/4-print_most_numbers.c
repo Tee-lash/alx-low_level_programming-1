aa74a16ac153bbf0aef61fbdb076b95fa44d6e22
Gbemividz0n <sporvida.jd@gmail.com>
2022-03-19 09:02:24 +0000

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers -print number
- *
- * Return: Always 0.
+ * print_most_numbers - prints the numbers without 2 and 4
  */
+
 void print_most_numbers(void)
 {
-int i;
+int x;
 
-for (i = 0; i < 10; i++)
+for (x = 48; x < 58; x++)
 {
-if (i != 2 && i != 4)
-{													_putchar (i + '0');
+if (x != 50 && x != 52)
+{
+													_putchar(x);
+													}
 }
-}												
-_putchar ('\n');
-
+_putchar(10);
 }
