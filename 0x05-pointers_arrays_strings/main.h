d082b6913e17946493d194b68bfa59e071ecb059
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-03-22 13:07:50 +0100

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef main
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,4 +13,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+#define main
 #endif
