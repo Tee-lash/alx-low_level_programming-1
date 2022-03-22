a2ed2ece61e0119eeec6746bcd4a3cc52a604d75
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-03-22 01:09:03 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - takes a string pointer as a parameter and adds a newline at the end
+ * @str: this is the string point
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
