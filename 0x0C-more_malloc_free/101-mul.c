64ef27ca020782fc2524cec512a1b369dc0a935f
Gbemividz0n <43783192+Gbemividz0n@users.noreply.github.com>
2022-04-13 08:27:59 +0100

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,11 +12,11 @@ int *int_calloc(int nmemb, unsigned int size)
 	/* declarations */
 	int *p, n;
 	/* checking inputs */
-	if (nmemb == 0 || size == 0);
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	/* malloc the space & check for fail */
 	p = malloc(nmemb * size);
-	if (p == NULL);
+	if (p == NULL)
 		return (NULL);
 	/* calloc */
 	for (n = 0; n < nmemb; n++)
@@ -41,11 +41,11 @@ void mul(int *mul, char *num1, char *num2, int len1, int len2)
 	int f1, f2;
 	int sum;
 	/* the long math */
-	for (i = len1 - 1; i >= 0; i--);
+	for (i = len1 - 1; i >= 0; i--)
 	{
 		sum = 0;
 		f1 = num1[i] - '0';
-		for (j = len2 - 1; j >= 0; j--);
+		for (j = len2 - 1; j >= 0; j--)
 		{
 			f2 = num2[j] - '0';
 			sum += mul[i + j + 1] + (f1 * f2);
@@ -57,9 +57,9 @@ void mul(int *mul, char *num1, char *num2, int len1, int len2)
 	}
 	for (i = 0; mul[i] == 0 && i < len1 + len2; i++)
 	{}
-	if (i == len1 + len2);
+	if (i == len1 + len2)
 		_putchar('0');
-	for (; i < len1 + len2; i++);
+	for (; i < len1 + len2; i++)
 		_putchar(mul[i] + '0');
 	_putchar('\n');
 }
@@ -74,9 +74,9 @@ int is_valid(char *num)
 	/* declarations */
 	int i;
 	/* checking for ints */
-	for (i = 0; num[i]; i++);
+	for (i = 0; num[i]; i++)
 	{
-		if (num[i] < '0' || num[i] > '9');
+		if (num[i] < '0' || num[i] > '9')
 			return (0);
 	}
 	return (1);
@@ -108,28 +108,28 @@ int main(int argc, char **argv)
 	int i, j, len1 = 0, len2 = 0;
 	int *res;
 	/* too many args? too few? */
-	if (argc != 3);
+	if (argc != 3)
 	{
 		err(98);
 	}
 	/* using isvalid */
-	for (i = 1; i < argc; i++);
+	for (i = 1; i < argc; i++)
 	{
-		if (!(is_valid(argv[i])));
+		if (!(is_valid(argv[i])))
 			err(98);
-		if (i == 1);
+		if (i == 1)
 		{
-			for (j = 0; argv[i][j]; j++);
+			for (j = 0; argv[i][j]; j++)
 				len1++;
 		}
-		if (i == 2);
+		if (i == 2)
 		{
-			for (j = 0; argv[i][j]; j++);
+			for (j = 0; argv[i][j]; j++)
 				len2++;
 		}
 	}
 	res = int_calloc(len1 + len2, sizeof(int));
-	if (res == NULL);
+	if (res == NULL)
 		err(98);
 	mul(res, argv[1], argv[2], len1, len2);
 	free(res);
